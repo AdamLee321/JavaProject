a1f9c6ab2ec9a3b1d63a030cbffea4477c60d7ba
George <pubx@outlook.com>
2015-02-27 16:44:45 +0000

    Done the connection part of the createDB class

--- src/model/Department.java
+++ src/model/Department.java
@@ -1,9 +1,45 @@
-package model;/*2ndYearProject
-  model
-  Created by David
-  18:36   25/02/2015
-  Software Development 3
-*/
+package model;
 
 public class Department {
+
+    private int deptId;
+    private String deptName;
+
+
+// default constructor
+    public Department(){
+
+        deptId = 0;
+        deptName = "";
+    }
+
+// overloaded constructor
+    public Department(String deptNameIn){  // also pass database sequence id
+
+        deptId = 0; // sequence in
+        deptName = deptNameIn;
+    }
+
+    // get department id
+    public int getDeptId(){
+
+        return this.deptId;
+    }
+
+    // get department name
+    public String getDeptName(){
+
+        return this.deptName;
+    }
+    // set department id
+    public void setDeptId(int deptIdIn){
+
+        deptId = deptIdIn;
+    }
+
+    // set department name
+    public void setDeptName(String deptNameIn){
+
+        deptName = deptNameIn;
+    }
 }
--- src/model/Member.java
+++ src/model/Member.java
@@ -1,9 +1,144 @@
-package model;/*2ndYearProject
-  model
-  Created by David
-  18:39   25/02/2015
-  Software Development 3
-*/
+package model;
 
 public class Member {
+
+// instance variables
+
+    private int memberId;
+    private String memberFName;
+    private String memberLName;
+    private String memberStreet;
+    private String memberCity;
+    private String memberCounty;
+    private String dob;
+    private String memberEmail;
+    private String memberNumber;
+    private int memberPoints;
+    private String memPicUrl;
+
+// default constructor
+
+    public Member(){
+
+        memberId = 0;
+        memberFName = "";
+        memberLName = "";
+        memberStreet = "";
+        memberCity = "";
+        memberCounty = "";
+        dob = "";
+        memberEmail = "";
+        memberNumber = "";
+        memberPoints = 0;
+        memPicUrl = "";
+    }
+
+// overloaded constructor NEEDS AN ID PASS FROM THE DATABASE
+
+    public Member(String memberFNameIn,String memberLNameIn,String memberStreetIn,String memberCityIn,String memberCountyIn,String dobIn,String memberEmailIn,String memberNumberIn,int memberPointsIn,String memPicUrlIn){
+
+        memberFName = memberFNameIn;
+        memberLName = memberLNameIn;
+        memberStreet = memberStreetIn;
+        memberCity = memberCityIn;
+        memberCounty = memberCountyIn;
+        dob = dobIn;
+        memberEmail = memberEmailIn;
+        memberNumber = memberNumberIn;
+        memberPoints = memberPointsIn;
+        memPicUrl = memPicUrlIn;
+    }
+
+// getters
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public String getMemberFName() {
+        return memberFName;
+    }
+
+    public String getMemberLName() {
+        return memberLName;
+    }
+
+    public String getMemberStreet() {
+        return memberStreet;
+    }
+
+    public String getMemberCity() {
+        return memberCity;
+    }
+
+    public String getMemberCounty() {
+        return memberCounty;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public String getMemberNumber() {
+        return memberNumber;
+    }
+
+    public int getMemberPoints() {
+        return memberPoints;
+    }
+
+    public String getMemPicUrl() {
+        return memPicUrl;
+    }
+
+// setters
+
+
+    public void setMemberId(int memberIdIn) {
+        this.memberId = memberIdIn;
+    }
+
+    public void setMemberFName(String memberFNameIn) {
+        this.memberFName = memberFNameIn;
+    }
+
+    public void setMemberLName(String memberLNameIn) {
+        this.memberLName = memberLNameIn;
+    }
+
+    public void setMemberStreet(String memberStreetIn) {
+        this.memberStreet = memberStreetIn;
+    }
+
+    public void setMemberCity(String memberCityIn) {
+        this.memberCity = memberCityIn;
+    }
+
+    public void setMemberCounty(String memberCountyIn) {
+        this.memberCounty = memberCountyIn;
+    }
+
+    public void setDob(String dobIn) {
+        this.dob = dobIn;
+    }
+
+    public void setMemberEmail(String memberEmailIn) {
+        this.memberEmail = memberEmailIn;
+    }
+
+    public void setMemberNumber(String memberNumberIn) {
+        this.memberNumber = memberNumberIn;
+    }
+
+    public void setMemberPoints(int memberPointsIn) {
+        this.memberPoints = memberPointsIn;
+    }
+
+    public void setMemPicUrl(String memPicUrlIn) {
+        this.memPicUrl = memPicUrlIn;
+    }
 }
