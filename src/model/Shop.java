00b5d311db07238b6014b661c42019f9f15c90b8
AdamLee123 <X00088467@gmail.com>
2015-02-28 09:54:11 +0000

    Update Shop.java

--- src/model/Shop.java
+++ src/model/Shop.java
@@ -18,7 +18,57 @@ public class Shop {
   private ArrayList<Department> departmentsArrayList;
 
   public Shop(String name, String street, String city, String county, String phoneNum){
-
+      this.shopId = shopId;
+      this.name = name;
+      this.street = street;
+      this.city = city;
+      this.county = county;
+      this.phoneNum = phoneNum;
   }
-
+  
+  //GETTERS
+  public int getShopId() {
+      return shopId;
+  }
+  
+  public String getName() {
+      return name;
+  }
+  
+  public String getStreet() {
+      return street;
+  }
+  
+  public String getCity() {
+      return city;
+  }
+  
+  public String getPhoneNum() {
+      return phoneNum;
+  }
+  
+  //SETTERS
+  public void setShopId(int shopId) {
+      this.shopId = shopId;
+  }
+  
+  public void setName(String name) {
+      this.name = name;
+  }
+  
+  public void setStreet(String street) {
+      this.street = street;
+  }
+  
+  public void setCity(String city) {
+      this.city = city;
+  }
+  
+  public void setCounty(String county) {
+      this.county = county;
+  }
+  
+  public void setPhoneNum(String phoneNum) {
+      this.phoneNum = phoneNum;
+  }
 }
