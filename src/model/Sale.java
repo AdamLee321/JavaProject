41d4728b5069988c8f02dde6aa192a9af932263d
AdamLee123 <X00088467@gmail.com>
2015-02-28 09:36:40 +0000

    Update Sale.java

--- src/model/Sale.java
+++ src/model/Sale.java
@@ -1,9 +1,41 @@
-package model;/*2ndYearProject
-  model
-  Created by David
-  18:38   25/02/2015
-  Software Development 3
+package model;
+
+/*
+IT Tallaght - 2015, S2
+Computing - Year 2, Project
+Group 17
 */
 
 public class Sale {
+  
+    private int saleID;
+    private String date;
+    private String time;
+    private double discount;
+    private double amount;
+    
+    public sale(){
+      saleID = 0;
+      date = "";
+      time = "";
+      discount = 0.00;
+      amount - 0.00;
+    }
+    
+    public Sale(int saleID, String date, String time, double discount, double amount) {
+      this.saleID = saleID;
+      this.date = date;
+      this.time = time;
+      this.discount = discount;
+      this.amount = amount;
+    }
+    
+    public void awardpoints(){
+      
+    }
+    
+    public double loyaltyDiscount(){
+      
+      return 0.00;
+    }
 }
