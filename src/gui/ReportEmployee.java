f888723ad344c19064f9a5a858bbb7333bae2b48
AdamLee123 <X00088467@gmail.com>
2015-03-10 16:38:16 +0000

    Update ReportEmployee.java

--- src/gui/ReportEmployee.java
+++ src/gui/ReportEmployee.java
@@ -14,19 +14,15 @@ Group 17
 
 public class ReportEmployee extends JFrame implements ActionListener{
 
-    //Declaration
-    private Button employee;
-    private Button sales;
-    private Button product;
-    private Button members;
-    private Button okButton;
-    private Button week;
-    private Button month;
-    private Button year;
+    public class ReportEmployee {
+
+    private MainFrame mf;
+    private JButton employee,year,month,week,okButton,members,product,sales;
     private JLabel jLabel1; //Graph
     private JPanel jPanel1; //Personnel Panel
     private JPanel jPanel2; //Calender
     private JPanel jPanel3; //Range
+    private Connection conn;
     private ReportOperations ro;
     private JPanel mainPanel;
     private JTextField jTextField1;
@@ -37,16 +33,85 @@ public class ReportEmployee extends JFrame implements ActionListener{
         return mainPanel;
     }
 
-    public JPanel getjPanel1(){
-        jPanel1 = new JPanel();
-        jPanel1.setBackground(new Color(98,168,221));
+    /*public ReportEmployee(MainFrame mf, Connection conn, ReportOperations ro) {
+        this.mf = mf;
+        this.conn = conn;
+        this.ro = ro;
+
+    }*/
+
+    public JPanel getjPanel1() {
+        jPanel3 = new JPanel(new GridBagLayout());
+        jPanel3.setBackground(new Color(98, 168, 221));
+
+        employee = new JButton("Employee");
+        employee.setVerticalTextPosition(SwingConstants.BOTTOM);
+        employee.setHorizontalTextPosition(SwingConstants.CENTER);
+        employee.addActionListener((ActionListener) this);
+
+        members = new JButton("Members");
+        members.setVerticalTextPosition(SwingConstants.BOTTOM);
+        members.setHorizontalTextPosition(SwingConstants.CENTER);
+        members.addActionListener((ActionListener) this);
+
+        product = new JButton("Product");
+        product.setVerticalTextPosition(SwingConstants.BOTTOM);
+        product.setHorizontalTextPosition(SwingConstants.CENTER);
+        product.addActionListener((ActionListener) this);
+
+        sales = new JButton("Sales");
+        sales.setVerticalTextPosition(SwingConstants.BOTTOM);
+        sales.setHorizontalTextPosition(SwingConstants.CENTER);
+        sales.addActionListener((ActionListener) this);
+
+        //Need to add to grid bag constraints
+
         return jPanel1;
     }
 
-    //Button Actions
-    private void employeeActionPerformed(ActionEvent e) {
-        //Void for now
+    public JPanel getjPanel2() {
+        jPanel2 = new JPanel(new GridBagLayout());
+        jPanel2.setBackground(new Color(98, 168, 221));
+
+        week = new JButton("Week");
+        week.setVerticalTextPosition(SwingConstants.BOTTOM);
+        week.setHorizontalTextPosition(SwingConstants.CENTER);
+        week.addActionListener((ActionListener) this);
+
+        month = new JButton("Month");
+        month.setVerticalTextPosition(SwingConstants.BOTTOM);
+        month.setHorizontalTextPosition(SwingConstants.CENTER);
+        month.addActionListener((ActionListener) this);
+
+        year = new JButton("Year");
+        year.setVerticalTextPosition(SwingConstants.BOTTOM);
+        year.setHorizontalTextPosition(SwingConstants.CENTER);
+        year.addActionListener((ActionListener) this);
+
+        //Need to add to grid bag constraints
+
+        return jPanel2;
     }
+
+    public JPanel getjPanel3() {
+        jPanel1 = new JPanel(new GridBagLayout());
+        jPanel1.setBackground(new Color(98, 168, 221));
+
+        okButton = new JButton("Ok");
+        okButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        okButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        okButton.addActionListener((ActionListener) this);
+
+        jTextField1 = new JTextField();
+        //
+
+        //Need to add to grid bag constraints
+
+        return jPanel3;
+    }
+
+
+
 
     private void salesActionPerformed(ActionEvent e) {
         //Void for now
@@ -76,10 +141,8 @@ public class ReportEmployee extends JFrame implements ActionListener{
     }
 
     public ReportEmployee(ReportOperations ro){
+
         this.ro = ro;
     }
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
-    }
 }
